20288f6e54830a7caf9958119f130590f681a7ec
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-10-01 01:33:45 +0300

    Rendering associated data in the json data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :response_not_found
+    def show
+        user = User.find(params[:id])
+        render json: user,include: :items
+    end
+
+    private
+
+    def response_not_found
+        render json: {error: "User not found"}, status: :not_found
+    end
 end
